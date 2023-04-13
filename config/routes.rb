f565b331bf5121841c95846a6cae3115662caa4e
Eric Oligney <eoengineer@gmail.com>
2023-04-13 12:18:34 -0400

    placeholder route, controller, spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'react#home'
+  
+  get '/react/home', to: 'react#home'
 end
