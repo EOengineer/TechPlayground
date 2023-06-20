c11e19d3f20ff02424ff934e479aa446e1891f65
Eric Oligney <eoengineer@gmail.com>
2023-06-20 15:38:36 -0400

    adds VCR and Webmock, cleans up gem groups.

--- Gemfile
+++ Gemfile
@@ -59,7 +59,6 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
   gem 'pry-rails'
-  gem 'rspec-rails', '~> 6.0.0'
   gem 'solargraph'
   gem 'solargraph-rails'
 end
@@ -73,4 +72,10 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+end
+
+group :test do
+  gem 'rspec-rails', '~> 6.0.0'
+  gem 'vcr'
+  gem "webmock"
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,8 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'vcr'
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
@@ -62,4 +64,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir = "spec/vcr"
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
 end
